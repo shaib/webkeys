9aaac63b8a2bf0765125c0a6d30cc955652f8aa3
Shai Berger <shai@platonix.com>
2012-05-03 19:27:40 +0300

    Use more modern template loaders

--- proj_pinax/settings.py
+++ proj_pinax/settings.py
@@ -96,8 +96,8 @@ SECRET_KEY = "2-jm9cf-ot1z5*&z(^n+ru$b#+lv3bmf27p6b$9g3er*c+4dl3"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
-    "django.template.loaders.filesystem.load_template_source",
-    "django.template.loaders.app_directories.load_template_source",
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
 ]
 
 MIDDLEWARE_CLASSES = [
