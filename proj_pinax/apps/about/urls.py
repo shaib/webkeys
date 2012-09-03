d8b4de23716cbd13f649f0c35fc857d901c811cd
Shai Berger <shai@platonix.com>
2012-09-03 20:05:45 +0300

    Started the about pages and download files, for real.

--- proj_pinax/apps/about/urls.py
+++ proj_pinax/apps/about/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
+def url_about_template(name):
+    regex = r"^%s/$" % name
+    params = dict(template="about/%s.html" % name)
+    return url(regex, direct_to_template, params, name=name)
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {"template": "about/about.html"}, name="about"),
-    url(r"^terms/$", direct_to_template, {"template": "about/terms.html"}, name="terms"),
-    url(r"^privacy/$", direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
-    url(r"^dmca/$", direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
-    url(r"^what_next/$", direct_to_template, {"template": "about/what_next.html"}, name="what_next"),
+#    url(r"^terms/$", direct_to_template, {"template": "about/terms.html"}, name="terms"),
+#    url(r"^privacy/$", direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
+#    url(r"^dmca/$", direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
+    url_about_template("what_next"),
+    url_about_template("use_xkb"),
+    url_about_template("use_klc"),
+    url_about_template("downloads"),
 )
